9749b632b2611742dccbffa415ecc6485d433293
rgarro <rolando@emptyart.xyz>
2016-10-25 22:43:34 -0600

    enabling CORS for device app

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'rack-cors', :require => 'rack/cors'
 gem 'mongoid',  github: 'mongoid/mongoid'
 gem 'bson_ext'
 
@@ -45,4 +45,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,21 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
-
+#Mongoid.load!('config/mongoid.yml')
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SharpfireRunnerxSite
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
